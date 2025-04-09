262a5ba8850a7f50fcbbaea74f2480bfd2169bb1
Vaibhav Vaishnav <vaibhavvaishnav221@gmail.com>
2025-04-09 18:23:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 let keys = Object.keys(window);
 
-console.log(keys);
+for (let i = 0; i < keys.length; i++) {
+    let value = window[keys[i]];
+    if (typeof value === "function") {
+        console.log(`Function: ${keys[i]}`);
+    } else if (typeof value === "object" && value !== null) {
+        console.log(`Object: ${keys[i]}`);
+    }
+}
